dda02732e64790cccce3ccb75b3ba28e17589d30
Jude <jude@gapp.nthu.edu.tw>
2022-04-27 03:08:07 +0000

    fix: 🐛 fix typescript errors

--- cms/config.js
+++ cms/config.js
@@ -1,36 +1,36 @@
 export default {
   cms_manual_init: true,
   backend: {
-    name: 'github',
-    repo: 'jakeprins/nextjs-netlify-cms',
-    branch: 'main',
+    name: "github",
+    repo: "jakeprins/nextjs-netlify-cms",
+    branch: "main",
   },
-  media_folder: 'public/img',
-  public_folder: 'img',
+  media_folder: "public/img",
+  public_folder: "img",
   collections: [
     {
-      name: 'pages',
-      label: 'Pages',
+      name: "pages",
+      label: "Pages",
       files: [
         {
-          label: 'Home',
-          name: 'home',
-          file: 'content/pages/home.md',
+          label: "Home",
+          name: "home",
+          file: "content/pages/home.md",
           fields: [
             {
-              label: 'Hero Title',
-              name: 'hero_title',
-              widget: 'string',
+              label: "Hero Title",
+              name: "hero_title",
+              widget: "string",
             },
             {
-              label: 'Hero Description',
-              name: 'hero_description',
-              widget: 'markdown',
+              label: "Hero Description",
+              name: "hero_description",
+              widget: "markdown",
             },
             {
-              label: 'Hero Image',
-              name: 'hero_image',
-              widget: 'image',
+              label: "Hero Image",
+              name: "hero_image",
+              widget: "image",
             },
           ],
         },
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,13 +1,57 @@
-import dynamic from 'next/dynamic';
-import config from '../cms/config';
-const CMS = dynamic(
-  () =>
-    import('netlify-cms-app').then((cms) => {
-      cms.init({ config });
-    }),
-  { ssr: false, loading: () => <p>Loading...</p> }
-);
-const AdminPage: React.FC = () => {
-  return <CMS />;
+import { useEffect } from "react";
+import type { NetlifyCmsApp } from "netlify-cms-app";
+import type { CmsConfig } from "netlify-cms-core";
+
+const config: CmsConfig = {
+  // cms_manual_init: true,
+  backend: {
+    name: "github",
+    repo: "judewang/nextjs-netlify-cms-github-backend",
+    branch: "main",
+  },
+  media_folder: "public/img",
+  public_folder: "img",
+  collections: [
+    {
+      name: "pages",
+      label: "Pages",
+      files: [
+        {
+          label: "Home",
+          name: "home",
+          file: "content/pages/home.md",
+          fields: [
+            {
+              label: "Hero Title",
+              name: "hero_title",
+              widget: "string",
+            },
+            {
+              label: "Hero Description",
+              name: "hero_description",
+              widget: "markdown",
+            },
+            {
+              label: "Hero Image",
+              name: "hero_image",
+              widget: "image",
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
-export default AdminPage;
+
+const Admin: React.FC = () => {
+  useEffect(() => {
+    async function loadCMS() {
+      const cms = await import("netlify-cms-app");
+      (cms as unknown as typeof NetlifyCmsApp)?.init({ config });
+    }
+    loadCMS();
+  }, []);
+
+  return null;
+};
+export default Admin;
